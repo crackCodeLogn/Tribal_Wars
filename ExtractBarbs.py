d9fee8171fe263d3df1d3fd6b2bf8f9cf3ff7d87
crackCodeLogN <taekwondo@Dan1>
2020-02-13 00:51:31 +0530

    Including Bonus villages in barb extraction

--- ExtractBarbs.py
+++ ExtractBarbs.py
@@ -17,44 +17,55 @@ class ExtractBarbsList:
         self.max_distance = max_distance
 
         self.BARB = 'Barbarian village'
+        self.BARB2 = 'Bonus village'
         self.FIRST_BRACE = '('
         self.BAR = '|'
         self.SECOND_BRACE = ')'
 
-    def _parse_for_coords(self, line):
-        line = line[line.index(self.BARB) + len(self.BARB):]
+    def _parse_for_coords(self, line, barb_type):
+        line = line[line.index(barb_type) + len(barb_type):]
         line = line[line.index(self.FIRST_BRACE) + 1: line.index(self.SECOND_BRACE)].strip()
         return line[:line.index(self.BAR)], line[line.index(self.BAR) + 1:]
+
+    def _perform_extraction(self, barb_type, line):
+        soup = BeautifulSoup(line)
+        rows = soup.findAll('tr')
+        barbs = []
+        for row in rows:
+            cells = row.findAll('td')
+            if len(cells) == 9:
+                distance, points, x, y = 0, 0, 0, 0
+                i = 0
+                for cell in cells:
+                    data = cell.text.strip()
+                    if i == 1:
+                        distance = float(data)
+                    elif i == 5:
+                        x, y = self._parse_for_coords(data, barb_type)
+                    elif i == 8:
+                        points = int(data)
+                    i += 1
+                villa = Villa(x, y, points=points, distance=distance)
+                if distance <= self.max_distance:
+                    barbs.append(villa)
+                else:
+                    print("Ignoring {} as distance {} > {}".format(villa, distance, self.max_distance))
+            else:
+                print('TW changed the order of showing barbs... Cannot proceed with info extraction')
+        return barbs
 
     def extract_barbs_from_site(self):
         req = requests.get(link)
         lines = req.text.split('\n')
         barbs = []
         for line in lines:
-            if self.BARB in line:
-                soup = BeautifulSoup(line)
-                rows = soup.findAll('tr')
-                for row in rows:
-                    cells = row.findAll('td')
-                    if len(cells) == 9:
-                        distance, points, x, y = 0, 0, 0, 0
-                        i = 0
-                        for cell in cells:
-                            data = cell.text.strip()
-                            if i == 1:
-                                distance = float(data)
-                            elif i == 5:
-                                x, y = self._parse_for_coords(data)
-                            elif i == 8:
-                                points = int(data)
-                            i += 1
-                        villa = Villa(x, y, points=points, distance=distance)
-                        if distance <= self.max_distance:
-                            barbs.append(villa)
-                        else:
-                            print("Ignoring {} as distance {} > {}".format(villa, distance, self.max_distance))
-                    else:
-                        print('TW changed the order of showing barbs... Cannot proceed with info extraction')
+            barb_type = None
+            if self.BARB in line: barb_type = self.BARB
+            if self.BARB2 in line: barb_type = self.BARB2
+            if barb_type:
+                obtained = self._perform_extraction(barb_type, line)
+                [barbs.append(barb) for barb in obtained]
+
         return barbs
 
     def read_in_villas_to_be_farmed(self, config):
