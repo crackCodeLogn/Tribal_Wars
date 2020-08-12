cf1517ef7c3b8e921c654c0374db772c55548a58
Vivek <vivektheintel@gmail.com>
2020-08-12 10:11:14 +0530

    Reffing static terms

--- util/VillaJsonGenerator.py
+++ util/VillaJsonGenerator.py
@@ -9,7 +9,8 @@ fp_input = input("Enter the file path containing the co-ordinates: ")
 with open(fp_input, 'r') as file:
     for line in file.readlines():
         spl = line.strip().split('|')
-        x, y, pts = spl[0], spl[1], -1
+        x, y = spl[0], spl[1]
+        pts, axe, lcav = -1, 0, 11
         print(villa_template
               .format(xc=x, yc=y, pts=pts, axe=0, lcav=11)
               .replace('^', '{')
