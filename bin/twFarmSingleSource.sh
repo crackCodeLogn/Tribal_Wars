7d908641797d72790d0838a05e46c5a2dfe96567
Vivek <vivektheintel@gmail.com>
2020-08-27 22:16:35 +0530

    Adding script to invoke directly from terminal

---
+++ bin/twFarmSingleSource.sh
@@ -0,0 +1,6 @@
+PYTHON_PATH=`pwd`'/Tribal_Wars'
+PYTHON_BASE='/usr/bin/python3.8'
+
+echo $PYTHON_PATH
+echo "Invoking NewSingleFarmingCommandCenter now!!"
+PYTHONPATH=$PYTHON_PATH $PYTHON_BASE $PYTHON_PATH/NewSingleFarmingCommandCenter.py
